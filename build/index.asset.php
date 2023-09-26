3a58393e7d9121e329ebd35065a47c0028f7f2b7
Carsten Bach <mail@carsten-bach.de>
2023-09-26 16:36:25 +0200

    BUILD

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n'), 'version' => '07441a6c1f264240d1d4');
+<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element'), 'version' => 'a0c19661cf6e82814df7');
